fffc1a70b522734304e249b3a1079f1732c2673c
Ivan Nikitin <nikitin.ivan.dev@gmail.com>
2021-06-06 15:05:53 +0100

    Upload to S3 bucket

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 extern crate argparse;
+extern crate dotenv;
 extern crate pcap;
+extern crate s3;
 
 use argparse::{ArgumentParser, Store, StoreTrue};
 use pcap::{Capture, Device};
+use s3::bucket::Bucket;
+use s3::creds::Credentials;
+use s3::S3Error;
+use std::env;
 use std::time::{SystemTime, UNIX_EPOCH};
+use tokio;
 
 fn show_devices(devices: &Vec<Device>) {
     for device in devices {
@@ -18,6 +25,24 @@ fn save_device(device_name: &str, requested_device: &mut Device, devices_vector:
             requested_device.desc = device.desc.clone();
         };
     }
+}
+
+#[tokio::main]
+async fn upload_to_s3(file_name: &str) -> Result<(), S3Error> {
+    dotenv::dotenv().expect("Failed to read .env file");
+    let access_key: String = env::var("ACCESS_KEY").expect("Could not read access key");
+    let secret_key: String = env::var("SECRET_KEY").expect("Could not read secret key");
+    let bucket_name = env::var("BUCKET_NAME").expect("BUCKET_NAME not found");
+    let region: s3::Region = s3::Region::EuWest1;
+    let credentials: Credentials =
+        Credentials::new(Some(&access_key), Some(&secret_key), None, None, None)
+            .expect("Could not init credentials");
+    let bucket: Bucket =
+        Bucket::new(&bucket_name, region, credentials).expect("Could not init s3 bucket");
+    let file: Vec<u8> = std::fs::read(&file_name).expect("Can not read file");
+    bucket.put_object(format!("/{}", &file_name), &file).await?;
+    println!("Uploaded to bucket: {}", bucket.name);
+    Ok(())
 }
 
 fn main() {
@@ -63,27 +88,30 @@ fn main() {
     let time_since_the_epoch = start_time
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards");
-    let start_time_in_seconds = time_since_the_epoch.as_secs();
+    let mut start_time_in_seconds = time_since_the_epoch.as_secs();
     let mut i = 1;
-    let mut file: pcap::Savefile = match captured_device.savefile(format!("dump{}.pcap", i)) {
+    let mut file_name = format!("dump{}.pcap", i);
+    let mut file: pcap::Savefile = match captured_device.savefile(file_name) {
         Ok(f) => f,
         Err(_) => std::process::exit(1),
     };
     while let Ok(packet) = captured_device.next() {
-        println!("Got a packet: {:?}", packet.header);
         file.write(&packet);
-        if SystemTime::now()
+        let current_time = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .expect("Time went backwards")
-            .as_secs()
-            - start_time_in_seconds
-            > 86400
-        {
-            file = match captured_device.savefile(format!("dump{}.pcap", i)) {
+            .as_secs();
+        if current_time - start_time_in_seconds > 60 {
+            file_name = format!("dump{}.pcap", i);
+            upload_to_s3(&file_name).expect("Failed to upload");
+            i += 1;
+            file_name = format!("dump{}.pcap", i);
+            println!("Writing to new file: {}", &file_name);
+            file = match captured_device.savefile(&file_name) {
                 Ok(f) => f,
                 Err(_) => std::process::exit(1),
             };
-            i += 1;
+            start_time_in_seconds = current_time;
         }
     }
 }
